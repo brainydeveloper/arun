874d2d23c6653a72885b3fef69a3718322831557
meta <brainydeveloper@gmail.com>
2013-12-11 16:26:31 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kk::Application.config.secret_key_base = '5651e25359a8f4cab0cfd951803b3893d1ffdfaf0894fa1d9b2bcbff7aa8bd5fa52a1d9a48c347f11b1169359372f647d934862312d1a0e5a062880b67ef6d65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kk::Application.config.session_store :cookie_store, key: '_kk_session'
